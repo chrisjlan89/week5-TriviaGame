041cf26630ed0b3c2ebc22da2c7138073733ab17
Chris Lantier <chrisj17@gmail.com>
2017-12-09 18:43:14 -0500

    sat night commit

--- quizattempt3.js
+++ quizattempt3.js
@@ -3,9 +3,13 @@
 window.onload = function() {
   $("#start-button").on("click", triviaGame.countDown );
   $("#submit").on("click", triviaGame.compareSubmit);
-  if(counter ==  3){
-     $("#submit").on("click", triviaGame.endGame)
+   $("#submit").on("click", triviaGame.endGame) 
+   // I thought this would make endGame call even if the user didn't cick submit
+   // on the last question. It did not work that way.
+   if(counter ==  3){
+      trivia.endGame();
    }
+   
 };
 
 
@@ -24,15 +28,20 @@ $("#qu1").hide();
 var answer;
 var triviaGame = {
   countDown : function(){
-    $("#question-display").empty();
-    timerDown = 31;
+ //  $("#question-display").empty();
+    timerDown = 3;
+    console.log("time down : " + timerDown)
     timerInterval = setInterval(function() {
       timerDown--
+
       $("#question-display").html("<div class =timer>" + timerDown + "</div>");
       if (timerDown <= 0){
+        console.log("clear intervaL called");
         clearInterval(timerInterval);
-      }}
-      , 1000);
+        triviaGame.compareSubmit();
+
+      }
+    }, 1000);
     if(counter == 0 ){
     $("#qu1").show();
     $("#submit").show();
@@ -47,13 +56,14 @@ var triviaGame = {
 
      compareSubmit : function() {
       console.log("qu" + counter);
-        var val = $('input[name=optradio]:checked').val();
-        alert(val);
+        var val = $('input[name=optradio' + counter+ ']:checked').val();
+       // alert(val);
+       console.log(val)
 
         if(val == answerList[counter]){
           rightGuess++;
           counter++;
-          triviaGame.countDown();
+          //triviaGame.countDown();
           console.log(rightGuess);
           clearInterval(timerInterval);
           console.log(counter)
@@ -63,10 +73,13 @@ var triviaGame = {
 
 
 
+
+
+
         else{
           counter++;
-          triviaGame.countDown();
-          clearInterval(timerInterval);
+          //triviaGame.countDown();
+           clearInterval(timerInterval);
            console.log(counter)
            $("#question-display").text("You got " + rightGuess + " / " + counter +"right.")
 
@@ -74,16 +87,24 @@ var triviaGame = {
 
         $("#qu" +  counter.toString()).hide();
         $("#qu" + (counter+1).toString()).show();
-      
+
+        var val;
+        triviaGame.countDown();
+
+        
      },
      
 
      endGame : function(){
+      if(counter ==  3){
+        $("#submit").hide();
+         clearInterval(timerInterval)
       $("#qu" +  counter.toString()).hide();
         $("#question-display").text("You got " + rightGuess + " / " + counter +"right.")
         $("#submit").on("click", triviaGame.endGame)
       
      }
+   }
    
 };
 
